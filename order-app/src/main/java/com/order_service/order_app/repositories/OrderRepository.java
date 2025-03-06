01c3262ccbd65dfe9aec5c5be48bfae1c7cd0bf9
Rafael Ferreira <rafabf18@gmail.com>
2025-03-06 10:53:32 -0300

    feat: adding repository

--- order-app/src/main/java/com/order_service/order_app/repositories/OrderRepository.java
+++ order-app/src/main/java/com/order_service/order_app/repositories/OrderRepository.java
@@ -1,11 +1,12 @@
 package com.order_service.order_app.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.order_service.order_app.models.Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Long, Order> {
-    
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 }
